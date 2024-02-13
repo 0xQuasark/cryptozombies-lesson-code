d7a1b70bcf0fc9aca5e440412b157ec8f75fb597
Quasark <q@quasark.xyz>
2024-02-13 13:29:19 -0800

    tuition, yay!

--- lesson-3/chapter-8/zombiehelper.sol
+++ lesson-3/chapter-8/zombiehelper.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.25;
+pragma solidity >=0.5.0 <0.6.0;
 import "./zombiefeeding.sol";
 contract ZombieHelper is ZombieFeeding {
 
@@ -7,4 +7,14 @@ contract ZombieHelper is ZombieFeeding {
     _;
   }
 
+  // Start here
+  function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    zombies[_zombieId].name = _newName;
+  }
+
+  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    zombies[_zombieId].dna = _newDna;
+  }
 }
