3dd7aec25807637b3956bf1a1835efb318ec9b92
Quasark <q@quasark.xyz>
2024-02-14 09:51:19 -0800

    more cz lessons

--- lesson-2/chapter-10/zombiefeeding.sol
+++ lesson-2/chapter-10/zombiefeeding.sol
@@ -1,5 +1,33 @@
 pragma solidity ^0.4.25;
 import "./zombiefactory.sol";
+// original kitty code:
+  // function getKitty(uint256 _id) external view returns (
+  //     bool isGestating,
+  //     bool isReady,
+  //     uint256 cooldownIndex,
+  //     uint256 nextActionAt,
+  //     uint256 siringWithId,
+  //     uint256 birthTime,
+  //     uint256 matronId,
+  //     uint256 sireId,
+  //     uint256 generation,
+  //     uint256 genes
+  // ) {
+  //     Kitty storage kit = kitties[_id];
+
+  //     // if this variable is 0 then it's not gestating
+  //     isGestating = (kit.siringWithId != 0);
+  //     isReady = (kit.cooldownEndBlock <= block.number);
+  //     cooldownIndex = uint256(kit.cooldownIndex);
+  //     nextActionAt = uint256(kit.cooldownEndBlock);
+  //     siringWithId = uint256(kit.siringWithId);
+  //     birthTime = uint256(kit.birthTime);
+  //     matronId = uint256(kit.matronId);
+  //     sireId = uint256(kit.sireId);
+  //     generation = uint256(kit.generation);
+  //     genes = kit.genes;
+  // }
+
 contract KittyInterface {
   function getKitty(uint256 _id) external view returns (
     bool isGestating,
--- lesson-2/chapter-6/zombiefactory.sol
+++ lesson-2/chapter-6/zombiefactory.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.4.25;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
 
 contract ZombieFactory {
 
@@ -17,19 +18,21 @@ contract ZombieFactory {
     mapping (uint => address) public zombieToOwner;
     mapping (address => uint) ownerZombieCount;
 
-    function _createZombie(string _name, uint _dna) private {
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;
-        zombieToOwner[id] = msg.sender;
+    function _createZombie(string memory _name, uint _dna) private {
+        // uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        // zombieToOwner[id] = msg.sender;
+        zombies.push(Zombie(_name, _dna));
+        uint id = zombies.length - 1;
         ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
     }
 
-    function _generateRandomDna(string _str) private view returns (uint) {
+    function _generateRandomDna(string memory _str) private view returns (uint) {
         uint rand = uint(keccak256(abi.encodePacked(_str)));
         return rand % dnaModulus;
     }
 
-    function createRandomZombie(string _name) public {
+    function createRandomZombie(string memory _name) public {
         require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
